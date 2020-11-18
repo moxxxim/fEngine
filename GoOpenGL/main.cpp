607ae424f546f749a974286151f7544c1398c088
Maksim Aliakseyeu <aliakseyeu.maxim@gmail.com>
2020-11-18 21:08:00 +0300

    Implement simple window

--- GoOpenGL/main.cpp
+++ GoOpenGL/main.cpp
@@ -39,12 +39,12 @@ namespace SMain
     GLFWwindow* CreateWindow()
     {
         constexpr int width = 800;
-        constexpr int height = 800;
-        GLFWwindow* window = glfwCreateWindow(width, height, "LearnOpenGL", nullptr, nullptr);
+        constexpr int height = 600;
+        GLFWwindow* window = glfwCreateWindow(width, height, "Sweet OpenGL Window", nullptr, nullptr);
         if (window != nullptr)
         {
             glfwMakeContextCurrent(window);
-            glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);  
+            glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
             glViewport(0, 0, width, height);
         }
         else
@@ -55,6 +55,24 @@ namespace SMain
 
         return window;
     }
+
+    void ProcessWindowEscapeInput(GLFWwindow &window)
+    {
+        if(glfwGetKey(&window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(&window, true);
+        }
+    }
+
+    void InitRender()
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    }
+
+    void Render()
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
 }
 
 int main(int argc, const char * argv[])
@@ -62,6 +80,18 @@ int main(int argc, const char * argv[])
     if(SMain::TryInitGlfw())
     {
         GLFWwindow *window = SMain::CreateWindow();
+        SMain::InitRender();
+
+        // Render loop.
+        while(!glfwWindowShouldClose(window))
+        {
+            SMain::ProcessWindowEscapeInput(*window);
+            SMain::Render();
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+
+        glfwTerminate();
         return 0;
     }
 
