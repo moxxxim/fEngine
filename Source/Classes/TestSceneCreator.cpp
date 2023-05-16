92e678db6afce8ba70d7e21b1bc0bdc3227a7a85
Maxim <aliakseyeu.maxim@gmail.com>
2023-05-17 00:45:39 +0300

    Fix cube geometry

--- Source/Classes/TestSceneCreator.cpp
+++ Source/Classes/TestSceneCreator.cpp
@@ -84,7 +84,7 @@ namespace test
             { Feng::Vector3(0.0f, 2.0f, 0.0f), Feng::Quaternion{}, vertexFormatPNU, "DiffTex1SpecTex2Material", false },
             { Feng::Vector3(2.0f, 2.0f, 0.0f), Feng::Quaternion{}, vertexFormatPNU, "BrickWallWithNmMaterial", false },
             { Feng::Vector3(4.0f, 2.0f, 0.0f), Feng::Quaternion{}, vertexFormatPNU, "BrickWallMaterial", false },
-            { Feng::Vector3(6.0f, 2.0f, 0.0f), Feng::Quat::MakeRotation(0, 90, 0), vertexFormatPNU, "BrickWallParallaxMaterial", false },
+            { Feng::Vector3(6.0f, 2.0f, 0.0f), Feng::Quaternion{}, vertexFormatPNU, "BrickWallParallaxMaterial", false },
             { Feng::Vector3( -2.0f, 1.0f, -4.0f), Feng::Quaternion{}, vertexFormatPNU, "DiffTex1SpecTex2Material", true },
             { Feng::Vector3( 0.0f, 1.0f, -4.0f), Feng::Quaternion{}, vertexFormatPNU, "TileWallMaterial", false },
             { Feng::Vector3( 2.0f, 1.0f, -4.0f), Feng::Quaternion{}, vertexFormatPNU, "DiffTex1SpecTex2Material", true },
