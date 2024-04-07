082ee970362778948aa52436b08d08e8cfe998cf
Maxim <aliakseyeu.maxim@gmail.com>
2024-04-08 00:00:14 +0300

    Make project work through Xcode on Mac

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,23 +1,28 @@
-#include <Feng/Core/Engine.hpp>
 #include <Classes/TestSceneCreator.hpp>
+#include <Feng/Core/Engine.hpp>
+#include <Feng/Utils/Debug.h>
 
 #include <iostream>
 
-#include <Feng/Utils/Debug.h>
-
 #ifdef WIN32
 #include <Windows.h>
+#elif defined(__APPLE__)
+#include <filesystem>
 #endif
 
 namespace SMain
 {
     void LogWorkingDir()
     {
+        std::string working_dir;
 #ifdef WIN32
         char buffer[MAX_PATH];
         ::GetCurrentDirectory(MAX_PATH, buffer);
-        Feng::Debug::LogMessage("[Feng] Start in directory: " + std::string{ buffer });
+        working_dir = std::string{ buffer };
+#elif defined(__APPLE__)
+        working_dir = std::filesystem::current_path().string();
 #endif
+        Feng::Debug::LogMessage("[Feng] Start in directory: " + working_dir);
     }
 }
 
