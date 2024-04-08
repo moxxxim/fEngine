6b897be502730d1ab0af4042d555564bc7fd761d
Maxim <aliakseyeu.maxim@gmail.com>
2024-04-08 13:15:05 +0300

    Some cmake improvements

--- CMakeUtils/FengCMakeUtils.cmake
+++ CMakeUtils/FengCMakeUtils.cmake
@@ -11,26 +11,19 @@ function(log_message_framed)
     log_delimeters()
 endfunction()
 
-function(demoArgs)
-      set(noValues     "")
-      set(singleValues SPECIAL)
-      set(multiValues  EXTRAS)
-      cmake_parse_arguments(
-          PARSE_ARGV 0
-          ARG
-          "${noValues}" "${singleValues}" "${multiValues}")
-
-      message("Left-over args: ${ARG_UNPARSED_ARGUMENTS}")
-      foreach(arg IN LISTS ARG_UNPARSED_ARGUMENTS)
-          message("${arg}")
-      endforeach()
-endfunction()
-
+# log list of messages each on a new line
 function(log_messages_optional)
     set(prefix ARG)
-    set(options      FRAME_UP FRAME_DOWN)
-    set(singleValues DO_LOG)
-    set(multiValues  MESSAGES)
+    set(options
+        FRAME_UP    # show frame before specified messages
+        FRAME_DOWN  # show frame after specified messages
+    )
+    set(singleValues 
+        DO_LOG      # specify whether messages should be logged
+    )
+    set(multiValues 
+        MESSAGES    # list of messages to log
+    )
 
     cmake_parse_arguments(
         PARSE_ARGV 0
@@ -66,11 +59,13 @@ function(log_messages_optional)
     endif()
 endfunction()
 
+# make a source files group 'group' in IDE for interface sources of a specified target
 function(source_group_from_target group target)
     get_target_property(srcFiles ${target} INTERFACE_SOURCES)
     source_group(${group} FILES ${srcFiles})
 endfunction()
 
+# make a source files group 'group' in IDE for files in directory 'dir' 
 function(source_group_from_dir group dir)
     file(GLOB_RECURSE srcFiles LIST_DIRECTORIES false
         "${dir}}/*.cpp"
